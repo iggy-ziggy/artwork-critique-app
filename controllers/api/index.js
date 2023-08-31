1607a97fe7d6ed8dcda3cacaba3ef7fe75d5ebe5
hershRVA <herchenrother@gmail.com>
2023-08-31 17:12:45 -0400

    changes to routes to make profile routes and artwork routes work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const profileRoutes = require('./profileRoutes');
+const profileroutes = require('./profileroutes');
 const artworkRoutes = require('./artworkRoutes');
 const tagRoutes = require('./tagRoutes');
 
 router.use('/users', userRoutes);
-router.use('/profiles', profileRoutes);
+router.use('/profiles', profileroutes);
 router.use('/artwork', artworkRoutes);
 router.use('/tags', tagRoutes);
 
