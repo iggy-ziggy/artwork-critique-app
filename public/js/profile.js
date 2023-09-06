b36db0063d5ea195498d8414ab592c62590e11a9
zeckenrode <zeckenrode9000@gmail.com>
2023-09-05 20:15:09 -0400

    fix redirect

--- public/js/profile.js
+++ public/js/profile.js
@@ -25,7 +25,7 @@ const newFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/api/profile');
     } else {
       alert('Failed to update profile');
     }
